37205217acf0b9c07aa11c7b22f6276fb5d46840
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-05-19 15:25:39 +0200

    [FEAT] scritta prima condizione ciclo for

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 const total_number = 100
+let fizz
 
 
 console.log(total_number)
 for (let i = 1; i <= total_number; i++){
+    if ((i % 3 === 0) && (i % 5 !== 0)) {
+        console.log(`${i} Fizz`)
+    }
 
-    console.log(i)
+    // console.log(i)
 }

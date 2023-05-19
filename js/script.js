e22ab72c3a28d6a2ed04ab3ce9fc5c0df3f1f66c
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-05-19 16:11:54 +0200

    aggiunto "use strict"; a file js

--- js/script.js
+++ js/script.js
@@ -1,32 +1,30 @@
-const total_number = 100
+"use strict";
 
-let tre = ' '
-let cinque = ' '
-let quindici = ' '
+const total_number = 100;
+
+let tre = ' ';
+let cinque = ' ';
+let quindici = ' ';
 
 
 for (let i = 1; i <= total_number; i++){
     if ((i % 3 === 0) && (i % 5 !== 0)) {
-        tre = 'Fizz'
-        console.log(tre)
+        tre = 'Fizz';
+        console.log(tre);
     }
 
     else if ((i % 3 !== 0) && (i % 5 === 0)) {
-        cinque = 'Buzz'
-        console.log(cinque)
+        cinque = 'Buzz';
+        console.log(cinque);
 
     } else {
         if((i % 3 === 0) && (i % 5 === 0)) {
-            quindici = 'FizzBuzz'
-            console.log(quindici)
+            quindici = 'FizzBuzz';
+            console.log(quindici);
         }
 
         else {
-            console.log(i)
+            console.log(i);
         }
-    }
-       
-
-
-    
-}
+    };   
+};

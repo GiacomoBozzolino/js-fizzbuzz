e2d7a1c0238bb93f77aa4bed2f89cd247a81949a
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-05-21 12:42:30 +0200

    [FEAT]fine lavoro stilizzazione js

--- Bonus 2/js/script.js
+++ Bonus 2/js/script.js
@@ -1,62 +1,49 @@
 const total_number = 100
-// let list = document.getElementById ('list')
+
 
 let tre = ' ';
 let cinque = ' ';
 let quindici = ' ';
 let board='';
 let number;
-const size= 10
+
 
 
 const output = document. getElementById('list')
 // INIZO CICLO FOR
+board +="<div class='flex'>"
+    for (let i = 1; i <= total_number; i++){
+            
+        // PRIMA CONDIZIONE
+        if ((i % 3 === 0) && (i % 5 !== 0)) {
+            tre = 'fizz'
+            board +=`<div class="square ${tre} shadow"> Fizz</div>`
+            console.log(tre)  
+        }
+        // SECONDA CONDIZIONE
+        else if ((i % 3 !== 0) && (i % 5 === 0)) {
+            cinque = 'buzz'
+            board +=`<div class="square ${cinque} shadow ">Buzz</div>`
+            console.log(cinque)
 
-
-
-    board +="<div class='flex'>"
-
-   
-
-        for (let i = 1; i <= total_number; i++){
-                // let new_number= document.createElement('div') 
-                // list.append (new_number)
-            // PRIMA CONDIZIONE
-            if ((i % 3 === 0) && (i % 5 !== 0)) {
-                tre = 'fizz'
-                board +=`<div class="square ${tre}"> Fizz</div>`
-                console.log(tre)
-               
-                // new_number.innerText = tre  
+        // TERZA CONDIZIONE
+        } else {
+            if((i % 3 === 0) && (i % 5 === 0)) {
+                quindici = 'fizzbuzz'
+                console.log(quindici)
+                board +=`<div class="square ${quindici} shadow">FizzBuzz</div>`
             }
-            // SECONDA CONDIZIONE
-            else if ((i % 3 !== 0) && (i % 5 === 0)) {
-                cinque = 'buzz'
-                console.log(cinque)
-                // new_number.innerText = cinque
-                board +=`<div class="square ${cinque}">Buzz</div>`
-                
-            // TERZA CONDIZIONE
-            } else {
-                if((i % 3 === 0) && (i % 5 === 0)) {
-                    quindici = 'fizzbuzz'
-                    console.log(quindici)
-                    // new_number.innerText = quindici 
-                    board +=`<div class="square ${quindici}">FizzBuzz</div>`
-                }
-                // QUARTA CONDIZIONE
-                else {
-                    console.log(i)
-                    // new_number.innerText = i 
-                    number = 'i'
-                    board +=`<div class="square ${number}"></div>`
-                }
+            // QUARTA CONDIZIONE
+            else {
+                let new_number = i
+                number = 'i'
+                console.log(new_number)
+                board +=`<div class="square ${number} shadow">${new_number}</div>`
             }
         }
+    }
     
-
-    board +='</div>'
-
+board +='</div>'
 
 output.innerHTML += board
 // FINE CICLO FOR
@@ -64,9 +51,3 @@ output.innerHTML += board
 
 
 
-// const size= 12;
-// let odd = '';
-// let even = '';
-// let board= '';
-
-// const
